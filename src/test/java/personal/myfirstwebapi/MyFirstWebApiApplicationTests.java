111e9e984c295de52565bab0b7f4ff294bbe78f0
Pedro Valravn <pedrovalravn@outlook.com>
2023-10-21 10:54:11 -0300

    initial commit

---
+++ src/test/java/personal/myfirstwebapi/MyFirstWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.myfirstwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
